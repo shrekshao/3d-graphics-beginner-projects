d8d0942e19b78df641713c80992d7d100178c668
shrekshao <shrekshao@gmail.com>
2023-03-28 10:59:54 -0700

    reorg

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,10 +3,11 @@ import { getSidebar } from 'vitepress-plugin-auto-sidebar'
 export default {
   // site-level options
   title: '3D Graphics Beginner Projects',
-  description: 'Just playing around.',
+  description: '',
 
   themeConfig: {
     // theme-level options
-    sidebar: getSidebar({ contentRoot: '/', contentDirs: ['src'], collapsible: true, collapsed: true })
+    displayAllHeaders: false,
+    sidebar: getSidebar({ contentRoot: '/', contentDirs: ['lessons', 'projects'], collapsible: false, collapsed: false })
   }
 }
