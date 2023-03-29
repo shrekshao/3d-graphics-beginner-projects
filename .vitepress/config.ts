489a052d47dbae3d806601b7c251f67728a5fd1b
shrekshao <shrekshao@gmail.com>
2023-03-29 12:45:21 -0700

    wip: i18n attempt (sidebar don't work)

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,5 +1,8 @@
 import { getSidebar } from 'vitepress-plugin-auto-sidebar'
 
+// TODO: use customized getSidebar func
+// This one doesn't work with i18n
+
 export default {
   // site-level options
   title: '3D Graphics Beginner Projects',
@@ -9,5 +12,22 @@ export default {
     // theme-level options
     displayAllHeaders: false,
     sidebar: getSidebar({ contentRoot: '/', contentDirs: ['lessons', 'projects'], collapsible: false, collapsed: false })
+  },
+
+  // vite: {
+  //   plugins: [
+  //     AutoSidebar,
+  //   ]
+  // },
+
+  locales: {
+    root: {
+      label: 'English',
+      lang: 'en'
+    },
+    zh: {
+      label: '简体中文',
+      lang: 'zh'
+    }
   }
 }
---
+++ zh/index.md
@@ -0,0 +1,17 @@
+---
+# layout: home
+
+title: 现代3D图形学实践作业（使用WebGPU）
+
+---
+
+# 现代3D图形学实践作业（使用WebGPU）
+
+施工中
+
+This is:
+  - A series of precanned projects for a 3D graphics new comer
+
+This is not:
+  - A thorough tutorial for the WebGPU API ([webgpufundamentals.org](https://webgpufundamentals.org/))
+  - A traditional univeristy style lecture-based course covering a more complete set of apsects of computer graphics ([UCSB GAMES 101](https://sites.cs.ucsb.edu/~lingqi/teaching/games101.html))
