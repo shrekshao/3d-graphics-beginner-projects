115192b4ca709ac0f2e2519d98d6d5d665a04438
shrekshao <shrekshao@gmail.com>
2023-03-29 12:53:26 -0700

    attempt use custom getsidebar

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,25 +1,59 @@
-import { getSidebar } from 'vitepress-plugin-auto-sidebar'
+import { defineConfig } from 'vitepress';
+import { getSidebar } from 'vitepress-plugin-auto-sidebar';
 
-// TODO: use customized getSidebar func
-// This one doesn't work with i18n
+// import fs from "fs";
+// import path from "path";
+// import process from 'node:process';
 
-export default {
-  // site-level options
+// interface Options {
+//   contentRoot?: string;
+//   contentDirs?: string[] | null;
+//   collapsible?: boolean;
+//   collapsed?: boolean;
+// }
+
+// function getSidebarItems(dir: string[], currentRoot: string | undefined, root: string | undefined, options: Options): object {
+// 	return dir.filter(e => e.endsWith('.md') || fs.statSync(path.resolve(currentRoot ?? '/', e)).isDirectory()).map((e: string) => {
+//     const childDir = path.resolve(currentRoot ?? '/', e);
+//     if (fs.statSync(childDir).isDirectory()) {
+//       return {
+//         text: (e.charAt(0).toUpperCase() + e.slice(1)).replaceAll('-', ' '),
+//         collapsible: options.collapsible,
+//         collapsed: options.collapsed,
+//         items: getSidebarItems(fs.readdirSync(childDir), childDir, root, options)
+//       };
+//     } else if (e.endsWith('.md')) {
+//       return {
+//         text: ((e.charAt(0).toUpperCase() + e.slice(1)).slice(0, -3)).replaceAll('-', ' '),
+//         link: childDir.replace(root ?? '', '')
+//       };
+//     }
+//     return {};
+//   })
+// };
+
+// function getSidebar(options: Options = {}) {
+//   options.contentRoot = options?.contentRoot ?? '/';
+//   options.contentDirs = options?.contentDirs ?? null;
+//   options.collapsible = options?.collapsible ?? true;
+//   options.collapsed = options?.collapsed ?? true;
+
+// 	options.contentRoot = path.join(process.cwd(), options.contentRoot)
+// 	const dir = fs.readdirSync(options.contentRoot).filter((file: string) => (options.contentDirs === null || options.contentDirs?.indexOf(file) !== -1) && fs.statSync(path.join(options.contentRoot ?? '/', file)).isDirectory());
+// 	return getSidebarItems(dir, options.contentRoot, options.contentRoot, options)
+// }
+
+export default defineConfig({
   title: '3D Graphics Beginner Projects',
   description: '',
-
   themeConfig: {
-    // theme-level options
-    displayAllHeaders: false,
-    sidebar: getSidebar({ contentRoot: '/', contentDirs: ['lessons', 'projects'], collapsible: false, collapsed: false })
+    sidebar: getSidebar({
+      contentRoot: '/',
+      contentDirs: ['lessons', 'projects'],
+      collapsible: false,
+      collapsed: false,
+    }) as any,
   },
-
-  // vite: {
-  //   plugins: [
-  //     AutoSidebar,
-  //   ]
-  // },
-
   locales: {
     root: {
       label: 'English',
@@ -29,5 +63,5 @@ export default {
       label: '简体中文',
       lang: 'zh'
     }
-  }
-}
+  },
+});
